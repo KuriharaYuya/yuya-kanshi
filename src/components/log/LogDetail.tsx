8f7b45c9f21044f3cf2020997e231bdff00df8e5
kurihara_yuya <nikoand01@gmail.com>
2023-05-13 12:18:15 +0900

    update: add button to back to home

--- src/components/log/LogDetail.tsx
+++ src/components/log/LogDetail.tsx
@@ -3,23 +3,33 @@ import MorningProps from "./detail/morning";
 import DietProps from "./detail/diet";
 import DeviceProps from "./detail/device";
 import styled from "./detail/section.module.scss";
+import { Button } from "@mui/material";
+import Router from "next/router";
 
 type Props = {
   logOutput: LogOutPut;
 };
 
 const DisplayLog = ({ logOutput }: Props) => {
+  const handleJumpToLogList = () => {
+    Router.push("/");
+  };
   return (
     <>
+      <Button onClick={handleJumpToLogList}>一覧ページへ</Button>
       <h1 style={{ textAlign: "center", margin: "0 auto" }}>
         {logOutput.date}の記録
       </h1>
-      <div style={{ textAlign: "center", margin: "0 auto" }}>
-        <a href={logOutput.tweetUrl}>twitterのURL</a>
-      </div>
+      <a
+        style={{ textAlign: "center", margin: "0 auto" }}
+        href={logOutput.tweetUrl}
+      >
+        twitterのURL
+      </a>
       <MorningProps morningProps={logOutput.mornings} />
       <DeviceProps deviceProps={logOutput.device} />
       <DietProps dietProps={logOutput.diet} />
+      <Button onClick={handleJumpToLogList}>一覧ページへ</Button>
     </>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,11 +25,9 @@ const DashBoard = ({ calenderData, tableData }: LogListOutPut) => {
   const { weekLabel, monthLabel, panel } = LabelDesigns;
 
   return (
-    <div style={{ margin: "0 auto" }}>
-      <div style={{ textAlign: "center" }}>
-        <h3>毎日継続カレンダー</h3>
-        <p>記録されたら黒くなります</p>
-      </div>
+    <>
+      <p style={{ textAlign: "center" }}>毎日継続カレンダー</p>
+      <p style={{ textAlign: "center" }}>記録されたら黒くなります</p>
       <Calender
         values={calenderData}
         until={until}
@@ -40,7 +38,7 @@ const DashBoard = ({ calenderData, tableData }: LogListOutPut) => {
       <br />
       <br />
       <LogTable logList={tableData} isAdmin={false} />
-    </div>
+    </>
   );
 };
 const LabelDesigns = {

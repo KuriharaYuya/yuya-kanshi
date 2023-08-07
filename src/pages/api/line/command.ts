43d06afdc919973af3db01b1ef55b9b8c19590ec
kurihara_yuya <nikoand01@gmail.com>
2023-08-08 00:13:33 +0900

    987656789

--- src/pages/api/line/command.ts
+++ src/pages/api/line/command.ts
@@ -26,13 +26,11 @@ const handler = async (
 
   if (commandProps.type === "ステータス") {
     await checkLogStatus(commandProps);
-    res.status(200).json({ message: "success" });
   } else if (commandProps.type === "投稿") {
-    res.status(200).json({ message: "success" });
     msg(`${commandProps.dateIso}のログを投稿中...`);
     await publishLog(commandProps);
   }
-  return;
+  return res.status(200).json({ message: "success" });
 };
 
 export default handler;

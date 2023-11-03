ae6a1dc7ce28b7ea7c41752e483fbb37a2b10044
kurihara_yuya <nikoand01@gmail.com>
2023-11-03 20:13:39 +0900

    lkjhgfgjkl

--- src/pages/api/tweet/_generateComment.ts
+++ src/pages/api/tweet/_generateComment.ts
@@ -1,11 +1,15 @@
 export const generateCommentData = () => {
-  const text = `この投稿は自動投稿です
-GitHubとアプリ自体のURL↓
-https://yuya-kanshi.vercel.app
-https://github.com/KuriharaYuya/yuya-kanshi#readme
-このアプリを使って下記を頑張ります！
-・インターン先での業務
-・大規模トラフィックの短・中期インターン採用通過のための対策
-法政でプログラミング興味あるよ！って方いたら繋がりましょう！`;
+  const text = `この投稿は自動投稿です。
+  この仕組みで自分の習慣を確立し
+  "2024年1~4月間の月売り上げ平均を100万円以上"
+  を実現します！
+  過去の習慣一覧は下記
+  https://www.notion.so/yuyanki/92a5712217cb4933968c5bfd19ee2c0d`;
+  return text;
+};
+
+export const generateCommentData2 = () => {
+  const text = `仕組み自体の説明は下記
+  https://github.com/KuriharaYuya/yuya-kanshi-serverless`;
   return text;
 };
